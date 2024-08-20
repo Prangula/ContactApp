c0b3cc82aae6baa1d6508b510fb3ea4f4ffc8f1c
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-08-20 18:09:20 +0400

    remove repository interface because of no Koin

--- app/src/main/java/com/myapplication/data/repository/ContactRepositoryImpl.kt
+++ app/src/main/java/com/myapplication/data/repository/ContactRepositoryImpl.kt
@@ -2,21 +2,17 @@ package com.myapplication.data.repository
 
 import com.myapplication.data.database.ContactDatabase
 import com.myapplication.data.database.ContactItem
-import com.myapplication.domain.repository.ContactRepository
 import kotlinx.coroutines.flow.Flow
 
-class ContactRepositoryImpl(private val db: ContactDatabase) : ContactRepository {
-    override suspend fun insert(contactItem: ContactItem) {
-        return db.contactDao().insert(contactItem)
-    }
+class ContactRepositoryImpl(private val db: ContactDatabase) {
 
-    override suspend fun update(contactItem: ContactItem) {
-        return db.contactDao().update(contactItem)
-    }
+    suspend fun insert(contactItem: ContactItem) =
+        db.contactDao().insert(contactItem)
 
-    override suspend fun getAllContacts(): Flow<List<ContactItem>> {
-        return db.contactDao().getAllContacts()
-    }
+    suspend fun update(contactItem: ContactItem) =
+        db.contactDao().update(contactItem)
 
-}
+    suspend fun getAllContacts() =
+        db.contactDao().getAllContacts()
+    }
 
