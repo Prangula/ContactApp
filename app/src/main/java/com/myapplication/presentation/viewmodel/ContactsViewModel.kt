d12e66fcd629e0cba77dc525c89e0baff7e0b8f1
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-08-20 18:28:37 +0400

    ViewModels and ContactsViewModel impl

---
+++ app/src/main/java/com/myapplication/presentation/viewmodel/ContactsViewModel.kt
@@ -0,0 +1,23 @@
+package com.myapplication.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.myapplication.data.database.ContactItem
+import com.myapplication.domain.use_cases.ContactsUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class ContactsViewModel(private val contactsUseCase: ContactsUseCase) : ViewModel() {
+
+    private val _contacts = MutableStateFlow<List<ContactItem>>(emptyList())
+    val contacts = _contacts.asStateFlow()
+
+    fun getAllContacts() {
+        viewModelScope.launch {
+            contactsUseCase().collect { item ->
+                _contacts.value = item
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/myapplication/presentation/viewmodel/UpdateViewModel.kt
@@ -0,0 +1,6 @@
+package com.myapplication.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class UpdateViewModel:ViewModel() {
+}
