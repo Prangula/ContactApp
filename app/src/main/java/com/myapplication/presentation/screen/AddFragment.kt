f6699ca9d4bcc141ad099d4e025dbb88a847f125
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-08-20 16:40:55 +0400

    Fragment Impl

---
+++ app/src/main/java/com/myapplication/presentation/screen/AddFragment.kt
@@ -0,0 +1,7 @@
+package com.myapplication.presentation.screen
+
+import androidx.fragment.app.Fragment
+import com.myapplication.R
+
+class AddFragment:Fragment(R.layout.fragment_add) {
+}
---
+++ app/src/main/java/com/myapplication/presentation/screen/EditFragment.kt
@@ -0,0 +1,7 @@
+package com.myapplication.presentation.screen
+
+import androidx.fragment.app.Fragment
+import com.myapplication.R
+
+class EditFragment:Fragment(R.layout.fragment_edit) {
+}
