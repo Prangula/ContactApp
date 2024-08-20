53b3971b103f6a82e80ab8c1fce051fa4ab5087e
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-08-20 18:10:45 +0400

    use cases

---
+++ app/src/main/java/com/myapplication/domain/use_cases/ContactsUseCase.kt
@@ -0,0 +1,12 @@
+package com.myapplication.domain.use_cases
+
+import com.myapplication.data.database.ContactItem
+import com.myapplication.data.repository.ContactRepositoryImpl
+import kotlinx.coroutines.flow.Flow
+
+class ContactsUseCase(private val contactRepositoryImpl: ContactRepositoryImpl) {
+
+    suspend operator fun invoke(): Flow<List<ContactItem>> {
+        return contactRepositoryImpl.getAllContacts()
+    }
+}
